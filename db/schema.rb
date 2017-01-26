cd815afd6180b26a1ed2a33b1932c9d4d4b17291
AyakaNozu <ayabon27@gmail.com>
2017-01-26 08:59:10 +0000

    add posting microposts

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
+ActiveRecord::Schema.define(version: 20170123125534) do
 ActiveRecord::Schema.define(version: 20170125073016) do
 
   create_table "microposts", force: :cascade do |t|
@@ -23,9 +23,6 @@ ActiveRecord::Schema.define(version: 20170125073016) do
 
   add_index "microposts", ["user_id", "created_at"], name: "index_microposts_on_user_id_and_created_at"
   add_index "microposts", ["user_id"], name: "index_microposts_on_user_id"
-=======
-ActiveRecord::Schema.define(version: 20170123125534) do
->>>>>>> user-profile
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
