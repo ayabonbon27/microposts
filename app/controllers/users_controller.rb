5cbb042aafe810059c74cd64bca1e30e5cbfeb69
AyakaNozu <ayabon27@gmail.com>
2017-01-28 08:58:52 +0000

    lesson7 8 submit2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,8 +2,8 @@ class UsersController < ApplicationController
   
   before_action :set_user, only: [:edit, :update]
   
-   def show # 追加
-   @user = User.find(params[:id])
+  def show # 追加
+    @user = User.find(params[:id])
     @microposts = @user.microposts.order(created_at: :desc)
   end
   
